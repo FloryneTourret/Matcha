f100162ca670c4d5dce953347c5938fb6fce038d
Floryne <floryne.tourret.pro@gmail.com>
2019-05-02 19:18:01 +0200

    rm bulma

--- views/Base/header_view.php
+++ views/Base/header_view.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Matcha</title>
 
-    <!-- <link rel="stylesheet" href="/assets/css/bulma.css"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/master.css">
     <link rel="stylesheet" href="/assets/css/style.css">
