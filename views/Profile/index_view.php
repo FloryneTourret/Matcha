3d22c1e96dd77490cfc6217d58a65d5dfeb896ea
Floryne <floryne.tourret.pro@gmail.com>
2019-05-08 13:54:47 +0200

    affichage age

--- views/Profile/index_view.php
+++ views/Profile/index_view.php
@@ -11,7 +11,15 @@
     </div>
     <div class="col-md-6">
         <h1 class="user_login"><?php if($user['user_gender_id'] == 1) echo '<i class="fas fa-mars"></i> '; else if($user['user_gender_id'] == 2) echo '<i class="fas fa-venus"></i> '; else if($user['user_gender_id'] == 3) echo '<i class="fas fa-mercury"></i> '; echo $user['login']?><?php if ($user['login'] == $_SESSION['user']['login']) echo '<a href="/index.php/Account" class="settings_user"><i class="fas fa-cog"></i></a>';?></h1>
-        <p><?php echo $user['firstname'].' '.$user['lastname'] ?></p>
+        <p>
+          <?php echo $user['firstname'].' '.$user['lastname'].', ';
+          $birthDate = $user['user_birthdate'];
+          $birthDate = explode("-", $birthDate);
+          $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[1], $birthDate[2], $birthDate[0]))) > date("md")
+            ? ((date("Y") - $birthDate[0]) - 1)
+            : (date("Y") - $birthDate[0]));
+          echo $age.' ans';
+        ?></p>
         <p>Orientation sexuelle : <i><?php echo $user['orientation_name'] ?></i></p>
         <?php if (!empty($user['biography'])){?>
             <p class="is-italic"><?php echo $user['biography']?></p>
@@ -23,8 +31,12 @@
     <nav>
       <div class="nav nav-tabs" id="nav-tab" role="tablist">
         <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Pictures</a>
+        
+        <?php if ($user['login'] ==  $_SESSION['user']['login']) { ?>
         <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Views</a>
         <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Likes</a>
+        <?php } ?>
+
       </div>
     </nav>
     <div class="tab-content" id="nav-tabContent">
@@ -55,6 +67,7 @@
             </div>
           <?php }$i++;} ?>
           </div>
+          <?php if (count($pictures) > 0){?>
           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
@@ -63,9 +76,13 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
+          <?php } else{ ?>
+            <p>Aucune photo actuellement.</p>
+          <?php } ?>
         </div>
       </div>
 
+      <?php if ($user['login'] ==  $_SESSION['user']['login']) { ?>
       <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
           Contenu views
       </div>
@@ -73,6 +90,7 @@
       <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
           Contenu likes
       </div>
+      <?php } ?>
     </div>
   </div>
 </div>
