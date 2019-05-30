fd115760ea11a774c8a6400bd9f7017d86d6b7bf
Frederic Leonard <fleonard@z4r7p3.le-101.fr>
2019-05-30 17:42:23 +0200

    accueil

--- controllers/Accueil.php
+++ controllers/Accueil.php
@@ -15,17 +15,48 @@ Class Accueil extends Controller{
 
     public function sort($type = 'match')
     {
-        $age_min = $_GET['age_min'];
-        $age_max = $_GET['age_max'];
-        $city = $_GET['city'];
-        $distance = $_GET['distance'];
-        $pop_min = $_GET['pop_min'];
-        $pop_max = $_GET['pop_max'];
-        $tags = $_GET['tags'];
+        if (!empty($_GET['age_min']) && is_numeric($_GET['age_min']))
+            $age_min = htmlspecialchars(addslashes($_GET['age_min']));
+        else
+            $age_min = 'none';
+        if (!empty($_GET['age_max']) && is_numeric($_GET['age_max']))
+            $age_max = htmlspecialchars(addslashes($_GET['age_max']));
+        else
+            $age_max = 'none';
+        if (!empty($_GET['city']))
+            $city = htmlspecialchars(addslashes($_GET['city']));
+        else
+            $city = 'none';
+        if (!empty($_GET['distance']) && is_numeric($_GET['distance']))
+            $distance = htmlspecialchars(addslashes($_GET['distance']));
+        else
+            $distance = 'none';
+        if (!empty($_GET['pop_min']) && is_numeric($_GET['pop_min']))
+            $pop_min = htmlspecialchars(addslashes($_GET['pop_min']));
+        else
+            $pop_min = 'none';
+        if (!empty($_GET['pop_max']) && is_numeric($_GET['pop_max']))
+            $pop_max = htmlspecialchars(addslashes($_GET['pop_max']));
+        else
+            $pop_max = 'none';
+        if (!empty($_GET['tags']))
+            $tags = $_GET['tags'];
+        else
+            $tags = 'none';
         if($tags != 'none')
             $tags = json_decode($tags);
-
+        $i = 0;
         $this->loadModel('Accueil_model');
+        if ($tags != 'none')
+        {
+            foreach($tags as $tag)
+            {
+                $tag = htmlspecialchars(addslashes($tag));
+                if ($this->Accueil_model->is_valid_tag($tag) == 0)
+                    array_splice($tags, $i, 1);
+                $i++;
+            }
+        }
         if(isset($_SESSION['user']['user_id']))
             $data['users'] = $this->Accueil_model->get_suggestions($type, $age_min, $age_max, $city, $distance, $pop_min, $pop_max, $tags);
         $this->loadView('Accueil/suggestions_view', $data);
