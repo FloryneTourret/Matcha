8dd198b5d44e59258c0aa68b47b8a73ce962e77c
Nathan Plouvier <nathanplouvier60@gmail.com>
2019-05-30 17:36:46 +0200

    update chat

--- controllers/Chat.php
+++ controllers/Chat.php
@@ -6,7 +6,7 @@ Class Chat extends Controller {
 		$this->loadModel('Chat_model');
 
 		$data = array (
-			'discutions' => $this->Chat_model->get_user_discutions($_SESSION['user']['user_id'])
+			'discussions' => $this->Chat_model->get_user_discussions($_SESSION['user']['user_id'])
 		);
 
 		$this->loadView('base/header_view');
@@ -15,18 +15,31 @@ Class Chat extends Controller {
 		$this->loadView('base/footer_view');
 	}
 
-	public function get_discution_messages()
+	public function get_discussion_messages()
 	{
-		if (isset($_GET['discution_id']) && isset($_SESSION['user']['user_id']))
+		if (isset($_GET['discussion_id']) && isset($_SESSION['user']['user_id']))
 		{
 			$this->loadModel('Chat_model');
 
-			$discution = $this->Chat_model->get_discution($_GET['discution_id']);
-			if (!empty($discution) && ($discution['first_user_id'] == $_SESSION['user']['user_id'] || $discution['second_user_id'] == $_SESSION['user']['user_id']))
+			$discussion = $this->Chat_model->get_discussion($_GET['discussion_id']);
+			if (!empty($discussion) && ($discussion['first_user_id'] == $_SESSION['user']['user_id'] || $discussion['second_user_id'] == $_SESSION['user']['user_id']))
 			{
-				$messages = $this->Chat_model->get_discution_messages($discution['discution_id']);
+				$messages = $this->Chat_model->get_discussion_messages($discussion['discussion_id']);
 				echo json_encode($messages);
 			}
+		}
+	}
+
+	public function post_message()
+	{
+		if (isset($_POST['discussion_id']) && is_numeric($_POST['discussion_id']) && isset($_POST['message_content']))
+		{
+			echo "ok";
+			$this->loadModel('Chat_model');
+
+			$this->Chat_model->new_message($_POST['discussion_id'], $_POST['message_content']);
+		} else {
+			echo "ko";
 		}
 	}
 }
--- models/Chat_model.php
+++ models/Chat_model.php
@@ -2,10 +2,10 @@
 
 Class Chat_model extends Model
 {
-	public function get_user_discutions($user_id)
+	public function get_user_discussions($user_id)
 	{
 		$stmt = $this->db->prepare("SELECT d.*, u1.login AS u1_login, u1.path_profile_picture AS u1_picture, u2.login AS u2_login, u2.path_profile_picture AS u2_picture
-									FROM user_discution d
+									FROM user_discussion d
 									INNER JOIN users u1 ON u1.user_id = d.first_user_id
 									INNER JOIN users u2 ON u2.user_id = d.second_user_id
 									WHERE u2.user_id = ? OR u1.user_id = ?");
@@ -13,23 +13,29 @@ Class Chat_model extends Model
 		return $stmt->fetchAll();
 	}
 
-	public function get_discution($discution_id)
+	public function get_discussion($discussion_id)
 	{
-		$stmt = $this->db->prepare("SELECT * FROM user_discution WHERE discution_id = ?");
-		$stmt->execute([$discution_id]);
+		$stmt = $this->db->prepare("SELECT * FROM user_discussion WHERE discussion_id = ?");
+		$stmt->execute([$discussion_id]);
 		return $stmt->fetch();
 	}
 
-	public function get_discution_messages($discution_id)
+	public function get_discussion_messages($discussion_id)
 	{
-		$stmt = $this->db->prepare("SELECT m.*, u.login, u.path_profile_picture FROM discution_messages m INNER JOIN users u ON u.user_id = m.user_id WHERE m.discution_id = ?");
-		$stmt->execute([$discution_id]);
+		$stmt = $this->db->prepare("SELECT m.*, u.login, u.path_profile_picture FROM discussion_messages m INNER JOIN users u ON u.user_id = m.user_id WHERE m.discussion_id = ?");
+		$stmt->execute([$discussion_id]);
 		return $stmt->fetchAll();
 	}
 
-	public function create_discution($user1_id, $user2_id)
+	public function create_discussion($user1_id, $user2_id)
 	{
-		$stmt = $this->db->prepare("INSERT INTO user_discution (`first_user_id`, `second_user_id`) VALUES (?, ?)");
+		$stmt = $this->db->prepare("INSERT INTO user_discussion (`first_user_id`, `second_user_id`) VALUES (?, ?)");
 		$stmt->execute([$user1_id, $user2_id]);
+	}
+
+	public function new_message($discussion_id, $message_content)
+	{
+		$stmt = $this->db->prepare("INSERT INTO `discussion_messages` (`user_id`, `discussion_id`, `message_content`) VALUES (?, ?, ?)");
+		return $stmt->execute([$_SESSION['user']['user_id'], $discussion_id, $message_content]);
 	}
 }
