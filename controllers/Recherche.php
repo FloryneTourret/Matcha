48f8529fca0fb4e658ab3917423a8d1603384c5e
Floryne <floryne.tourret.pro@gmail.com>
2019-05-30 17:20:07 +0200

    recherche

--- controllers/Recherche.php
+++ controllers/Recherche.php
@@ -6,66 +6,72 @@ Class Recherche extends Controller{
         $this->loadModel('Recherche_model');
         $data['tags'] = $this->Recherche_model->get_tags();
         
-        if (!empty($_POST['orientation']) || !empty($_POST['latitude']) ||
-            !empty($_POST['min_age']) || !empty($_POST['max_age']) || 
-            !empty($_POST['distance']) || !empty($_POST['tags']) ||
-            !empty($_POST['min_pop']) || !empty($_POST['max_pop']))
-        {
-            if (!empty($_POST['min_age']) && !empty($_POST['max_age']))
-            {
-                $min = htmlspecialchars(addslashes($_POST['min_age']));
-                $max = htmlspecialchars(addslashes($_POST['max_age']));
+        $this->loadView('Base/header_view');
+        $this->loadView('Base/navbar_view');
+        $this->loadView('Recherche/index_view',$data);
+        $this->loadView('Base/footer_view');
+    }
+
+    public function sort($type = 'classic')
+    {
+        $this->loadModel('Recherche_model');
+        $data = array();
+        if (
+            !empty($_GET['orientation']) || !empty($_GET['latitude']) ||
+            !empty($_GET['min_age']) || !empty($_GET['max_age']) ||
+            !empty($_GET['distance']) || !empty($_GET['tags']) ||
+            !empty($_GET['min_pop']) || !empty($_GET['max_pop'])
+        ) {
+            if (!empty($_GET['min_age']) && !empty($_GET['max_age'])) {
+                $min = htmlspecialchars(addslashes($_GET['min_age']));
+                $max = htmlspecialchars(addslashes($_GET['max_age']));
                 if ($min > $max)
                     $data['error'] = "L'age minimum est supérieur à l'âge maximum";
+            } else if (!empty($_GET['min_age']))
+                $min = htmlspecialchars(addslashes($_GET['min_age']));
+            else if (!empty($_GET['max_age']))
+                $max = htmlspecialchars(addslashes($_GET['max_age']));
+            else {
+                $min = 'none';
+                $max = 'none';
             }
-            else if (!empty($_POST['min_age']))
-                $min = htmlspecialchars(addslashes($_POST['min_age']));
-            else if (!empty($_POST['max_age']))
-                $max = htmlspecialchars(addslashes($_POST['max_age']));
-            else
-            {
-                $min = 0;
-                $max = 2018;
-            }
-            if (!empty($_POST['orientation']))
-                $orientation = htmlspecialchars(addslashes($_POST['orientation']));
-            if (!empty($_POST['locality']))
-                $city = htmlspecialchars(addslashes($_POST['locality']));
+            if (!empty($_GET['orientation']))
+                $orientation = htmlspecialchars(addslashes($_GET['orientation']));
+            if (!empty($_GET['city']))
+                $city = htmlspecialchars(addslashes($_GET['city']));
             else
                 $city = 0;
-            if (!empty($_POST['distance']))
-                $distance  = htmlspecialchars(addslashes($_POST['distance']));
+            if (!empty($_GET['distance']))
+                $distance  = htmlspecialchars(addslashes($_GET['distance']));
             else
-                $distance = 2147483647;
-            $tags = array();
-            if (!empty($_POST['tags'][0]))
-            {
+                $distance = 'none';
+            $tags = json_decode($_GET['tags']);
+            if (!empty( $tags[0])) {
                 $i = 0;
-                foreach($_POST['tags'] as $tag)
-                {
+                foreach ($tags as $tag) {
                     $tags[$i] = htmlspecialchars(addslashes($tag));
                     $i++;
                 }
             }
-            if (!empty($_POST['min_pop']) && !empty($_POST['max_pop'])) {
-                $min_pop = htmlspecialchars(addslashes($_POST['min_pop']));
-                $max_pop = htmlspecialchars(addslashes($_POST['max_pop']));
-                if ( $min_pop > $max_pop)
+            else{
+                $tags = 'none';
+            }
+            if (!empty($_GET['min_pop']) && !empty($_GET['max_pop'])) {
+                $min_pop = htmlspecialchars(addslashes($_GET['min_pop']));
+                $max_pop = htmlspecialchars(addslashes($_GET['max_pop']));
+                if ($min_pop > $max_pop)
                     $data['error'] = "La popularité minimum est supérieur à la popularité maximum";
-            } else if (!empty($_POST['min_pop']))
-                $min_pop = htmlspecialchars(addslashes($_POST['min_pop']));
-            else if (!empty($_POST['max_pop']))
-                $max_pop = htmlspecialchars(addslashes($_POST['max_pop']));
+            } else if (!empty($_GET['min_pop']))
+                $min_pop = htmlspecialchars(addslashes($_GET['min_pop']));
+            else if (!empty($_GET['max_pop']))
+                $max_pop = htmlspecialchars(addslashes($_GET['max_pop']));
             else {
                 $min_pop = 0;
                 $max_pop = 5000;
             }
-            $data['search'] = $this->Recherche_model->get_users_from_search($_SESSION['user']['genre'], $min, $max, $orientation, $city, $distance, $tags, $min_pop, $max_pop);
+            $data['users'] = $this->Recherche_model->get_users_from_search($_SESSION['user']['user_gender_id'],$type, $min, $max, $orientation, $city, $distance, $tags, $min_pop, $max_pop);
         }
-        $this->loadView('Base/header_view');
-        $this->loadView('Base/navbar_view');
-        $this->loadView('Recherche/recherche_view',$data);
-        $this->loadView('Base/footer_view');
+        $this->loadView('Accueil/suggestions_view', $data);
     }
 }
 
