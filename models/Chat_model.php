6b89e3e2f04ad2d07a874370a3de6917a5c40245
Floryne Tourret <ftourret@z4r7p1.le-101.fr>
2019-06-04 14:59:56 +0200

    ne pas afficher messages si bloque

--- models/Chat_model.php
+++ models/Chat_model.php
@@ -5,14 +5,15 @@ Class Chat_model extends Model
 	public function get_user_discussions($user_id)
 	{
 		$stmt = $this->db->prepare("SELECT d.*, (SELECT message_content FROM discussion_messages WHERE discussion_id = d.discussion_id ORDER BY message_date DESC LIMIT 1) AS last_message, 
-		(SELECT lu FROM discussion_messages WHERE discussion_id = d.discussion_id ORDER BY message_date DESC LIMIT 1) AS lu,
-		(SELECT `user_id` FROM discussion_messages WHERE discussion_id = d.discussion_id ORDER BY message_date DESC LIMIT 1) AS last_message_user,
-		u1.login AS u1_login, u1.path_profile_picture AS u1_picture, u2.login AS u2_login, u2.path_profile_picture AS u2_picture 
-		FROM user_discussion d 
-		INNER JOIN users u1 ON u1.user_id = d.first_user_id 
-		INNER JOIN users u2 ON u2.user_id = d.second_user_id 
-		WHERE u2.user_id = ? OR u1.user_id = ?
-		");
+        (SELECT lu FROM discussion_messages WHERE discussion_id = d.discussion_id ORDER BY message_date DESC LIMIT 1) AS lu,
+        (SELECT `user_id` FROM discussion_messages WHERE discussion_id = d.discussion_id ORDER BY message_date DESC LIMIT 1) AS last_message_user,
+        u1.login AS u1_login, u1.path_profile_picture AS u1_picture, u2.login AS u2_login, u2.path_profile_picture AS u2_picture 
+        FROM user_discussion d 
+        INNER JOIN users u1 ON u1.user_id = d.first_user_id 
+        INNER JOIN users u2 ON u2.user_id = d.second_user_id 
+        LEFT OUTER JOIN `user_blacklist` b1 on u2.user_id = b1.id_user_blacklisted
+        LEFT OUTER JOIN `user_blacklist` b2 on u1.user_id = b2.id_user_blacklisted
+        WHERE b1.id_user_blacklist is NULL AND b2.id_user_blacklist is NULL AND b1.id_user_blacklisted is NULL AND b2.id_user_blacklisted is NULL AND (u2.user_id = ? OR u1.user_id = ?)");
 		$stmt->execute([$user_id, $user_id]);
 		return $stmt->fetchAll();
 	}
